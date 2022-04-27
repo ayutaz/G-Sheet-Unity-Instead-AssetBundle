a2c1d85d43ee68d519ed5c395e3163d6de757a86
ayutaz <ka1357amnbpdr@gmail.com>
2022-04-27 15:21:12 +0900

    feat: Create a batch of characters SO.

---
+++ Assets/_Project/Scripts/Editor/CreateCharacterSO.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+
+namespace _Project.Editor
+{
+    public class CreateCharacterSO : EditorWindow
+    {
+        private const int maxSOCount = 30;
+        [MenuItem("Tools/Create Character SO")]
+        private static void CreateSO()
+        {
+            for (var index = 0; index < maxSOCount; index++)
+            {
+                var instance = CreateInstance<CharacterSO>();
+                AssetDatabase.CreateAsset(instance, $"Assets/_Project/CharacterData/Character{index}.asset");
+                AssetDatabase.Refresh();
+            }
+        }
+    }
+}
