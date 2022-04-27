5ff74fa43db016899ee6d0e1225fb102eacb2419
ayutaz <ka1357amnbpdr@gmail.com>
2022-04-27 15:20:38 +0900

    feat: add character so class

---
+++ Assets/_Project/Scripts/CharacterSO.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace _Project
+{
+    [CreateAssetMenu(fileName = "Character", menuName = "Character", order = 0)]
+    public class CharacterSO : ScriptableObject
+    {
+        public CharacterInfo CharacterInfo;
+    }
+}
